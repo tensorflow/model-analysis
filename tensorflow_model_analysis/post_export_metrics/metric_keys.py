3716807e0b225d0e4b2415fa7f9e64886dccc818
Shivam Jindal <jindalshivam09@gmail.com>
2019-03-15 16:26:59 -0700

    Revert "Fixing comment in metric_keys.py"

--- tensorflow_model_analysis/post_export_metrics/metric_keys.py
+++ tensorflow_model_analysis/post_export_metrics/metric_keys.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Library defining metric keys for post export metrics.
+"""Library defining metric keys for post export evaluation.
 
 These keys defines the name of the post export metrics that are supported by
 TFMA.
