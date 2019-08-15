64da8eef52255f7dab6f249d5fc992e59794d541
tf-model-analysis-team <tensorflow-extended-team@google.com>
2019-08-15 10:25:24 -0700

    Introduce tfma-config-picker.
    PiperOrigin-RevId: 263590299

---
+++ tensorflow_model_analysis/frontend/tfma-config-picker/tfma-config-picker.js
@@ -0,0 +1,230 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+import {template} from './tfma-config-picker-template.html.js';
+
+import '../tfma-multi-select/tfma-multi-select.js';
+
+/**
+ * tfma-config-picker allows the user to select the desired config from a nested
+ * structure.
+ *
+ * @polymer
+ */
+export class ConfigPicker extends PolymerElement {
+  constructor() {
+    super();
+  }
+
+  static get is() {
+    return 'tfma-config-picker';
+  }
+
+  /** @return {!HTMLTemplateElement} */
+  static get template() {
+    return template;
+  }
+
+  /** @return {!PolymerElementProperties} */
+  static get properties() {
+    return {
+      /**
+       * A map where the keys are output names and the values are list of
+       * classes for that output.
+       * @type {!Object<!Array<number|string>>}
+       */
+      allConfigs: {type: Object},
+
+      /**
+       * The list of all outputs.
+       * @private {!Array<string>}
+       */
+      availableOutputs_: {
+        type: Array,
+        computed: 'computeAvailableOutputs_(allConfigs)',
+        observer: 'availableOutputsChanged_',
+      },
+
+      /**
+       * The list of selected outputs.
+       * @private {!Array<string>}
+       */
+      selectedOutputs_: {
+        type: Array,
+        value: () => [],
+      },
+
+      availableCombos_: {
+        type: Array,
+        computed: 'computeAvailableCombos_(allConfigs, selectedOutputs_)',
+      },
+
+      /**
+       * The values to be rendered. Each element represents a row. If the
+       * original data is not a 1d array, the values are turned into a string
+       * like "[[1, 2, 3], [4,5,6]]".
+       * @type {!Array<number|string>}
+       */
+      availableClasses_: {
+        type: Array,
+        computed: 'computeAvailableClasses_(availableCombos_)',
+        observer: 'availableClassesChanged_',
+      },
+
+      /**
+       * The list of selected classes.
+       * @private {!Array<string>}
+       */
+      selectedClasses_: {type: Array},
+
+      /**
+       * An object representing selected config. The keys are the name of the
+       * output and the values are the class ids selected for that output.
+       * @private {!Object<number|string>}
+       */
+      selectedConfigs: {
+        type: Object,
+        computed: 'computeSelectedConfigs_(' +
+            'availableCombos_, availableClasses_, selectedClasses_)',
+        notify: true,
+      }
+    };
+  }
+
+  /**
+   * Determines the list of available outputs.
+   * @param {!Object} allConfigs
+   * @return {!Array<string>}
+   * @private
+   */
+  computeAvailableOutputs_(allConfigs) {
+    return Object.keys(allConfigs).sort();
+  }
+
+  /**
+   * Observer for the property data. It will update the arrayData property.
+   * @param {!Array<string>} availableOutputs
+   * @private
+   */
+  availableOutputsChanged_(availableOutputs) {
+    // Clears selected classes when available outputs changed.
+    this.selectedClasses_ = [];
+
+    // If there is only one output, select it automatically.
+    if (availableOutputs.length == 1) {
+      this.selectedOutputs_ = [availableOutputs[0]];
+    }
+  }
+
+  /**
+   * Determines all available combinations of configurations.
+   * @param {!Object<!Array<string|number>>} allConfigs
+   * @param {!Array<string>} selectedOutputs
+   * @return {!Array<!Object>}
+   * @private
+   */
+  computeAvailableCombos_(allConfigs, selectedOutputs) {
+    const availableCombo = [];
+    // If there are more than one output, we should prepend output name to help
+    // differentiate bewteen the same class ids from different outputs.
+    const prependOutputName = selectedOutputs.length > 1;
+
+    selectedOutputs.forEach(outputName => {
+      const classes = allConfigs[outputName] || [];
+      classes.forEach(classId => {
+        availableCombo.push({
+          outputName: outputName,
+          classId: classId,
+          prependOutputName: prependOutputName,
+        });
+      });
+    });
+
+    return availableCombo;
+  }
+
+  /**
+   * Builds the list of available classes from all possible configuration.
+   * @param {!Array<!Object>} availableCombos
+   * @return {!Array<string>}
+   * @private
+   */
+  computeAvailableClasses_(availableCombos) {
+    const maybeAddOutputPrefix = (combo) => combo.prependOutputName ?
+        (combo.outputName || 'Empty Output') + ', C: ' :
+        '';
+    const determineClassIdToDisplay = (combo) =>
+        (combo.classId == tfma.NO_CLASS_ID ? 'No class' : combo.classId);
+
+    return availableCombos.reduce((acc, combo) => {
+      acc.push(maybeAddOutputPrefix(combo) + determineClassIdToDisplay(combo));
+      return acc;
+    }, []);
+  }
+
+  /**
+   * Observer for property availableClassses.
+   * @param {!Array<string>} availableClasses
+   * @private
+   */
+  availableClassesChanged_(availableClasses) {
+    // If there is only one class, select it automatically.
+    if (availableClasses.length == 1) {
+      this.selectedClasses_ = [availableClasses[0]];
+    }
+  }
+
+  /**
+   * Builds the selected config from what available and what's selected.
+   * @param {!Array<!Object>} availableCombos
+   * @param {!Array<string>} availableClasses
+   * @param {!Array<string>} selectedClasses
+   * @return {!Object<!Object<string>>}
+   * @private
+   */
+  computeSelectedConfigs_(availableCombos, availableClasses, selectedClasses) {
+    const config = {};
+    if (availableCombos && availableClasses && selectedClasses) {
+      selectedClasses.forEach(selectedClass => {
+        // Use index lookup to avoid parsing the generated strings which might
+        // contain output names.
+        const index = availableClasses.indexOf(selectedClass);
+        const selectedCombo = availableCombos[index];
+        if (selectedCombo) {
+          const outputName = selectedCombo.outputName;
+          if (config[outputName]) {
+            config[outputName].push(selectedCombo.classId);
+          } else {
+            config[outputName] = [selectedCombo.classId];
+          }
+        }
+      });
+    }
+    return config;
+  }
+
+  /**
+   * @param {!Array} array
+   * @return {boolean} True if the given array has more than one element.
+   * @private
+   */
+  show_(array) {
+    return !!array && array.length > 1;
+  }
+}
+
+customElements.define('tfma-config-picker', ConfigPicker);
---
+++ tensorflow_model_analysis/frontend/tfma-config-picker/tfma-config-picker_test.js
@@ -0,0 +1,202 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+suite('tests', () => {
+  /**
+   * Test component element.
+   * @type {!Element}
+   */
+  let element;
+
+  test('testSingleOutputNoClass', done => {
+    element = fixture('element');
+    const singleOutputNioClassConfig = {'': ['nci']};
+    element.allConfigs = singleOutputNioClassConfig;
+
+    const verifyNoDropdown = () => {
+      // No dropdown should be present since there is only one output and
+      // no class id.
+      assert.isNull(element.shadowRoot.querySelector('tfma-multi-select'));
+      setTimeout(checkSelectedConfigs, 1);
+    };
+
+    const checkSelectedConfigs = () => {
+      // The only output and no class should be selected automatically.
+      assert.deepEqual(element.selectedConfigs, singleOutputNioClassConfig);
+      done();
+    };
+
+    setTimeout(verifyNoDropdown, 0);
+  });
+
+  test('testSingleOutputMultiClass', done => {
+    element = fixture('element');
+    element.allConfigs = {'': ['0', '1', '2']};
+    let dropdown;
+
+    const verifyDropdownAvailable = () => {
+      // Class id dropdown should be present since we will automatically select
+      // the only avialable output.
+      dropdown = element.shadowRoot.querySelector('tfma-multi-select');
+      assert.isNotNull(dropdown);
+      assert.equal(dropdown.label, 'Class Ids');
+      setTimeout(chooseClass2, 1);
+    };
+
+    const chooseClass2 = () => {
+      dropdown.selectIndex(2);
+      setTimeout(checkClass2Selected, 1);
+    };
+
+    const checkClass2Selected = () => {
+      assert.deepEqual(element.selectedConfigs, {'': ['2']});
+      setTimeout(chooseClass0, 0);
+    };
+
+    const chooseClass0 = () => {
+      dropdown.selectIndex(0);
+      setTimeout(checkClass0Selected, 1);
+    };
+
+    const checkClass0Selected = () => {
+      assert.deepEqual(element.selectedConfigs, {'': ['2', '0']});
+      done();
+    };
+
+    setTimeout(verifyDropdownAvailable, 0);
+  });
+
+  test('testMultiOutputNoClass', done => {
+    element = fixture('element');
+    element.allConfigs = {'output1': ['nci'], 'output2': ['nci']};
+    let outputDropdown;
+    let classIdsDropdown;
+
+    const checkOutputDropdownAvailable = () => {
+      // Class id dropdown should not be present since we will automatically
+      // select the only avialable output.
+      outputDropdown = element.shadowRoot.querySelector('tfma-multi-select');
+      assert.isNotNull(outputDropdown);
+      assert.equal(outputDropdown.label, 'Outputs');
+      setTimeout(chooseOutput1, 1);
+    };
+
+    const chooseOutput1 = () => {
+      outputDropdown.selectIndex(0);
+      setTimeout(checkOutput1Selected, 1);
+    };
+
+    const checkOutput1Selected = () => {
+      assert.deepEqual(element.selectedConfigs, {'output1': ['nci']});
+      setTimeout(checkClassIdsDropdownNotAvailable, 1);
+    };
+
+    const checkClassIdsDropdownNotAvailable = () => {
+      const dropdowns =
+          element.shadowRoot.querySelectorAll('tfma-multi-select');
+      assert.equal(dropdowns.length, 1);
+      assert.equal(outputDropdown, dropdowns[0]);
+      setTimeout(chooseOutput2, 1);
+    };
+
+    const chooseOutput2 = () => {
+      outputDropdown.selectIndex(1);
+      setTimeout(checkNoConfigSelected, 1);
+    };
+
+    const checkNoConfigSelected = () => {
+      // The selected config is removed since there are more than one
+      // posibilities now.
+      const dropdowns =
+          element.shadowRoot.querySelectorAll('tfma-multi-select');
+      assert.equal(dropdowns[0], outputDropdown);
+      classIdsDropdown = dropdowns[1];
+      assert.equal(classIdsDropdown.label, 'Class Ids');
+      setTimeout(selectOutput2NoClass, 1);
+    };
+
+    const selectOutput2NoClass = () => {
+      classIdsDropdown.selectIndex(1);
+      setTimeout(checkOutput2Selected, 1);
+    };
+
+    const checkOutput2Selected = () => {
+      assert.deepEqual(element.selectedConfigs, {'output2': ['nci']});
+      done();
+    };
+
+    setTimeout(checkOutputDropdownAvailable, 0);
+  });
+
+  test('testMultiOutputMultiClass', done => {
+    element = fixture('element');
+    element.allConfigs = {
+      'output1': ['0', '1', '2'],
+      'output2': ['0', '2', '4']
+    };
+    let outputDropdown;
+    let classIdsDropdown;
+
+    const selectOutput1 = () => {
+      outputDropdown = element.shadowRoot.querySelector('tfma-multi-select');
+      outputDropdown.selectIndex(0);
+      setTimeout(selectOutput2, 1);
+    };
+
+    const selectOutput2 = () => {
+      outputDropdown.selectIndex(1);
+      setTimeout(selectOutput1Class0, 1);
+    };
+
+    const selectOutput1Class0 = () => {
+      const dropdowns =
+          element.shadowRoot.querySelectorAll('tfma-multi-select');
+      assert.equal(dropdowns.length, 2);
+      assert.equal(outputDropdown, dropdowns[0]);
+      classIdsDropdown = dropdowns[1];
+      classIdsDropdown.selectIndex(0);
+      setTimeout(checkOutput1Class0Selected, 1);
+    };
+
+    const checkOutput1Class0Selected = () => {
+      assert.deepEqual(element.selectedConfigs, {'output1': ['0']});
+      setTimeout(selectOutput2Class2And4, 1);
+    };
+
+    const selectOutput2Class2And4 = () => {
+      classIdsDropdown.selectIndex(4);
+      classIdsDropdown.selectIndex(5);
+      setTimeout(checkOutput2Class2And4Selected, 1);
+    };
+
+    const checkOutput2Class2And4Selected = () => {
+      assert.deepEqual(
+          element.selectedConfigs, {'output1': ['0'], 'output2': ['2', '4']});
+      setTimeout(unselectOutput1, 1);
+    };
+
+    const unselectOutput1 = () => {
+      outputDropdown.selectIndex(0);
+      setTimeout(allSelectionCleared, 1);
+    };
+
+    const allSelectionCleared = () => {
+      assert.deepEqual(element.selectedConfigs, {});
+      done();
+    };
+
+    setTimeout(selectOutput1, 0);
+  });
+});
