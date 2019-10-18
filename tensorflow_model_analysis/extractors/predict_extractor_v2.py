d1d826ec789dbe60382c969a3ad283bf6756faee
mdreves <mdreves@google.com>
2019-10-17 20:13:28 -0700

    Updated PredictExtractor V2 to support multiple models and customized signatures.
    PiperOrigin-RevId: 275391978

--- tensorflow_model_analysis/extractors/predict_extractor_v2.py
+++ tensorflow_model_analysis/extractors/predict_extractor_v2.py
@@ -18,15 +18,17 @@ from __future__ import division
 # Standard __future__ imports
 from __future__ import print_function
 
+import collections
 import copy
 
 import apache_beam as beam
 import tensorflow as tf
+from tensorflow_model_analysis import config
 from tensorflow_model_analysis import constants
 from tensorflow_model_analysis import model_util
 from tensorflow_model_analysis import types
 from tensorflow_model_analysis.extractors import extractor
-from typing import Generator, List, Optional
+from typing import Iterable, List
 
 PREDICT_EXTRACTOR_STAGE_NAME = 'ExtractPredictions'
 
@@ -34,19 +36,20 @@ PREDICT_SIGNATURE_DEF_KEY = 'predict'
 
 
 def PredictExtractor(
-    eval_shared_model: types.EvalSharedModel,
-    desired_batch_size: Optional[int] = None) -> extractor.Extractor:
+    eval_config: config.EvalConfig,
+    eval_shared_models: List[types.EvalSharedModel]) -> extractor.Extractor:
   """Creates an extractor for performing predictions.
 
-  The extractor's PTransform loads and runs the serving saved_model against
-  every extact yielding a copy of the incoming extracts with an additional
+  The extractor's PTransform loads and runs the serving saved_model(s) against
+  every extract yielding a copy of the incoming extracts with an additional
   extract added for the predictions keyed by tfma.PREDICTIONS_KEY. The model
   inputs are searched for under tfma.FEATURES_KEY (keras only) or tfma.INPUT_KEY
-  (if tfma.FEATURES_KEY is not set or the model is non-keras).
+  (if tfma.FEATURES_KEY is not set or the model is non-keras). If multiple
+  models are used the predictions will be stored in a dict keyed by model name.
 
   Args:
-    eval_shared_model: Shared model parameters.
-    desired_batch_size: Optional batch size for batching.
+    eval_config: Eval config.
+    eval_shared_models: Shared model parameters.
 
   Returns:
     Extractor for extracting predictions.
@@ -55,8 +58,7 @@ def PredictExtractor(
   return extractor.Extractor(
       stage_name=PREDICT_EXTRACTOR_STAGE_NAME,
       ptransform=_ExtractPredictions(
-          eval_shared_model=eval_shared_model,
-          desired_batch_size=desired_batch_size))
+          eval_config=eval_config, eval_shared_models=eval_shared_models))
 
 
 @beam.typehints.with_input_types(beam.typehints.List[types.Extracts])
@@ -64,133 +66,158 @@ def PredictExtractor(
 class _PredictionDoFn(model_util.DoFnWithModels):
   """A DoFn that loads the models and predicts."""
 
-  def __init__(self, eval_shared_model: types.EvalSharedModel) -> None:
-    super(_PredictionDoFn, self).__init__({'': eval_shared_model.model_loader})
+  def __init__(self, eval_config: config.EvalConfig,
+               eval_shared_models: List[types.EvalSharedModel]) -> None:
+    super(_PredictionDoFn, self).__init__(
+        {m.model_path: m.model_loader for m in eval_shared_models})
+    self._eval_config = eval_config
     self._predict_batch_size = beam.metrics.Metrics.distribution(
         constants.METRICS_NAMESPACE, 'predict_batch_size')
     self._predict_num_instances = beam.metrics.Metrics.counter(
         constants.METRICS_NAMESPACE, 'predict_num_instances')
 
   def process(
-      self, batch_of_extracts: List[types.Extracts]
-  ) -> Generator[types.Extracts, None, None]:
+      self,
+      batch_of_extracts: List[types.Extracts]) -> Iterable[types.Extracts]:
     batch_size = len(batch_of_extracts)
     self._predict_batch_size.update(batch_size)
     self._predict_num_instances.inc(batch_size)
 
-    # TODO(b/141016373): Add support for multiple models.
-    loaded_model = self._loaded_models['']
-    signatures = None
-    if loaded_model.keras_model:
-      signatures = loaded_model.keras_model.signatures
-    elif loaded_model.saved_model:
-      signatures = loaded_model.saved_model.signatures
-    if not signatures:
-      raise ValueError(
-          'PredictExtractor V2 requires a keras model or a serving model. '
-          'If using EvalSavedModel then you must use PredictExtractor V1.')
+    output_batch_of_extracts = []
+    for spec in self._eval_config.model_specs:
+      if spec.location not in self._loaded_models:
+        raise ValueError('loaded model for location {} not found: '
+                         'locations={}, eval_config={}'.format(
+                             spec.location, self._loaded_models.keys(),
+                             self._eval_config))
+      loaded_model = self._loaded_models[spec.location]
+      signatures = None
+      if loaded_model.keras_model:
+        signatures = loaded_model.keras_model.signatures
+      elif loaded_model.saved_model:
+        signatures = loaded_model.saved_model.signatures
+      if not signatures:
+        raise ValueError(
+            'PredictExtractor V2 requires a keras model or a serving model. '
+            'If using EvalSavedModel then you must use PredictExtractor V1.')
 
-    # First try 'predict' then try 'serving_default'. The estimator output for
-    # the 'serving_default' key does not include all the heads in a multi-head
-    # model. However, keras only uses the 'serving_default' for its outputs.
-    # Note that the 'predict' key only exists for estimators for multi-head
-    # models, for single-head models only 'serving_default' is used.
-    signature_key = tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
-    if PREDICT_SIGNATURE_DEF_KEY in signatures:
-      signature_key = PREDICT_SIGNATURE_DEF_KEY
-    if signature_key not in signatures:
-      raise ValueError('{} not found in model signatures: {}'.format(
-          signature_key, signatures))
-    signature = signatures[signature_key]
+      signature_key = spec.signature_name
+      if (not signature_key and spec.signature_names and
+          spec.model_name in spec.signature_names):
+        signature_key = spec.signature_names[spec.model_name]
+      if not signature_key:
+        # First try 'predict' then try 'serving_default'. The estimator output
+        # for the 'serving_default' key does not include all the heads in a
+        # multi-head model. However, keras only uses the 'serving_default' for
+        # its outputs.  Note that the 'predict' key only exists for estimators
+        # for multi-head models, for single-head models only 'serving_default'
+        # is used.
+        signature_key = tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
+        if PREDICT_SIGNATURE_DEF_KEY in signatures:
+          signature_key = PREDICT_SIGNATURE_DEF_KEY
+      if signature_key not in signatures:
+        raise ValueError('{} not found in model signatures: {}'.format(
+            signature_key, signatures))
+      signature = signatures[signature_key]
 
-    # If input names exist then filter the inputs by these names (unlike
-    # estimators, keras does not accept unknown inputs).
-    input_names = None
-    if self._loaded_models[''].keras_model is not None:
-      input_names = self._loaded_models[''].keras_model.input_names
-    # First arg of structured_input_signature tuple is shape, second is dtype
-    # (we currently only support named params passed as a dict)
-    elif (signature.structured_input_signature and
-          len(signature.structured_input_signature) == 2 and
-          isinstance(signature.structured_input_signature[1], dict)):
-      input_names = [name for name in signature.structured_input_signature[1]]
-    if input_names is not None:
-      # TODO(b/138474171): Make this code more efficient.
-      inputs = {}
-      found = []
-      for name in input_names:
-        inputs[name] = []
-        for extract in batch_of_extracts:
-          # If features key exist, use that for features, else use input_key
-          if constants.FEATURES_KEY in extract:
-            input_features = extract[constants.FEATURES_KEY]
-          else:
-            input_features = extract[constants.INPUT_KEY]
-          if isinstance(input_features, dict):
-            if name in input_features:
-              found.append(name)
-              inputs[name].append(input_features[name])
-          else:
-            if len(inputs) > 1:
-              raise ValueError('PredictExtractor passed single input but keras '
-                               'model expects multiple: keras.input_names = %s,'
-                               ' extracts = %s' % (input_names, extract))
-            found.append(name)
-            inputs[name].append(input_features)
-      if len(found) != len(input_names):
-        tf.compat.v1.logging.warning(
-            'PredictExtractor inputs do not match those expected by the '
-            'model: input_names = %s, found in extracts = %s' %
-            (input_names, found))
-    else:
-      # Assume serialized examples
-      inputs = [extract[constants.INPUT_KEY] for extract in batch_of_extracts]
+      # If input names exist then filter the inputs by these names (unlike
+      # estimators, keras does not accept unknown inputs).
+      input_names = None
+      if loaded_model.keras_model is not None:
+        input_names = loaded_model.keras_model.input_names
+      # First arg of structured_input_signature tuple is shape, second is dtype
+      # (we currently only support named params passed as a dict)
+      elif (signature.structured_input_signature and
+            len(signature.structured_input_signature) == 2 and
+            isinstance(signature.structured_input_signature[1], dict)):
+        input_names = [name for name in signature.structured_input_signature[1]]
+      if input_names is not None:
+        # TODO(b/138474171): Make this code more efficient.
+        inputs = collections.defaultdict(list)
+        found = {}
+        for name in input_names:
+          for extract in batch_of_extracts:
+            # If features key exist, use that for features, else use input_key
+            if constants.FEATURES_KEY in extract:
+              input_features = extract[constants.FEATURES_KEY]
+            else:
+              input_features = extract[constants.INPUT_KEY]
+            if isinstance(input_features, dict):
+              if name in input_features:
+                found[name] = True
+                inputs[name].append(input_features[name])
+            else:
+              if len(inputs) > 1:
+                raise ValueError(
+                    'PredictExtractor passed single input but keras model '
+                    'expects multiple: model.input_names = {}, '
+                    'extracts={}'.format(input_names, extract))
+              found[name] = True
+              inputs[name].append(input_features)
+        if len(found) != len(input_names):
+          tf.compat.v1.logging.warning(
+              'PredictExtractor inputs do not match those expected by the '
+              'model: input_names={}, found in extracts={}'.format(
+                  input_names, found))
+      else:
+        # Assume serialized examples
+        inputs = [extract[constants.INPUT_KEY] for extract in batch_of_extracts]
 
-    if isinstance(inputs, dict):
-      predictions = signature(**{k: tf.constant(v) for k, v in inputs.items()})
-    else:
-      predictions = signature(tf.constant(inputs, dtype=tf.string))
+      if isinstance(inputs, dict):
+        outputs = signature(**{k: tf.constant(v) for k, v in inputs.items()})
+      else:
+        outputs = signature(tf.constant(inputs, dtype=tf.string))
 
-    for i in range(batch_size):
-      prediction = {k: v[i].numpy() for k, v in predictions.items()}
-      # Keras and regression serving models return a dict of predictions even
-      # for single-outputs. Convert these to a single tensor for compatibility
-      # with the labels (and model.predict API).
-      if len(prediction) == 1:
-        prediction = list(prediction.values())[0]
-      extracts = copy.copy(batch_of_extracts[i])
-      extracts[constants.PREDICTIONS_KEY] = prediction
-      yield extracts
+      for i in range(batch_size):
+        output = {k: v[i].numpy() for k, v in outputs.items()}
+        # Keras and regression serving models return a dict of predictions even
+        # for single-outputs. Convert these to a single tensor for compatibility
+        # with the labels (and model.predict API).
+        if len(output) == 1:
+          output = list(output.values())[0]
+        if i >= len(output_batch_of_extracts):
+          output_batch_of_extracts.append(copy.copy(batch_of_extracts[i]))
+        # If only one model, the predictions are stored without using a dict
+        if len(self._eval_config.model_specs) == 1:
+          output_batch_of_extracts[i][constants.PREDICTIONS_KEY] = output
+        else:
+          if constants.PREDICTIONS_KEY not in output_batch_of_extracts[i]:
+            output_batch_of_extracts[i][constants.PREDICTIONS_KEY] = {}
+          output_batch_of_extracts[i][constants.PREDICTIONS_KEY][spec.name] = (
+              output)
+
+    return iter(output_batch_of_extracts)
 
 
 @beam.ptransform_fn
 @beam.typehints.with_input_types(types.Extracts)
 @beam.typehints.with_output_types(types.Extracts)
 def _ExtractPredictions(  # pylint: disable=invalid-name
-    extracts: beam.pvalue.PCollection,
-    eval_shared_model: types.EvalSharedModel,
-    desired_batch_size: Optional[int] = None) -> beam.pvalue.PCollection:
+    extracts: beam.pvalue.PCollection, eval_config: config.EvalConfig,
+    eval_shared_models: List[types.EvalSharedModel]) -> beam.pvalue.PCollection:
   """A PTransform that adds predictions and possibly other tensors to extracts.
 
   Args:
     extracts: PCollection of extracts containing model inputs keyed by
       tfma.FEATURES_KEY (if model inputs are named) or tfma.INPUTS_KEY (if model
       takes raw tf.Examples as input).
-    eval_shared_model: Shared model parameters.
-    desired_batch_size: Optional batch size for prediction.
+    eval_config: Eval config.
+    eval_shared_models: Shared model parameters.
 
   Returns:
     PCollection of Extracts updated with the predictions.
   """
   batch_args = {}
-  if desired_batch_size:
+  if eval_config.options.HasField('desired_batch_size'):
     batch_args = dict(
-        min_batch_size=desired_batch_size, max_batch_size=desired_batch_size)
+        min_batch_size=eval_config.options.desired_batch_size.value,
+        max_batch_size=eval_config.options.desired_batch_size.value)
 
   extracts = (
       extracts
       | 'Batch' >> beam.BatchElements(**batch_args)
       | 'Predict' >> beam.ParDo(
-          _PredictionDoFn(eval_shared_model=eval_shared_model)))
+          _PredictionDoFn(
+              eval_config=eval_config, eval_shared_models=eval_shared_models)))
 
   return extracts
--- tensorflow_model_analysis/extractors/predict_extractor_v2_test.py
+++ tensorflow_model_analysis/extractors/predict_extractor_v2_test.py
@@ -24,6 +24,7 @@ import apache_beam as beam
 from apache_beam.testing import util
 import numpy as np
 import tensorflow as tf
+from tensorflow_model_analysis import config
 from tensorflow_model_analysis import constants
 from tensorflow_model_analysis.api import model_eval_lib
 from tensorflow_model_analysis.eval_saved_model import testutil
@@ -46,9 +47,13 @@ class PredictExtractorTest(testutil.TensorflowModelAnalysisTest):
     export_dir, _ = (
         fixed_prediction_estimator_extra_fields
         .simple_fixed_prediction_estimator_extra_fields(temp_export_dir, None))
+
+    eval_config = config.EvalConfig(
+        model_specs=[config.ModelSpec(location=export_dir)])
     eval_shared_model = self.createTestEvalSharedModel(
         eval_saved_model_path=export_dir, tags=[tf.saved_model.SERVING])
-    predict_extractor = predict_extractor_v2.PredictExtractor(eval_shared_model)
+    predict_extractor = predict_extractor_v2.PredictExtractor(
+        eval_config=eval_config, eval_shared_models=[eval_shared_model])
 
     examples = [
         self._makeExample(
@@ -100,9 +105,12 @@ class PredictExtractorTest(testutil.TensorflowModelAnalysisTest):
     export_dir, _ = dnn_classifier.simple_dnn_classifier(
         temp_export_dir, None, n_classes=2)
 
+    eval_config = config.EvalConfig(
+        model_specs=[config.ModelSpec(location=export_dir)])
     eval_shared_model = self.createTestEvalSharedModel(
         eval_saved_model_path=export_dir, tags=[tf.saved_model.SERVING])
-    predict_extractor = predict_extractor_v2.PredictExtractor(eval_shared_model)
+    predict_extractor = predict_extractor_v2.PredictExtractor(
+        eval_config=eval_config, eval_shared_models=[eval_shared_model])
 
     examples = [
         self._makeExample(age=1.0, language='english', label=0),
@@ -139,9 +147,12 @@ class PredictExtractorTest(testutil.TensorflowModelAnalysisTest):
     export_dir, _ = dnn_classifier.simple_dnn_classifier(
         temp_export_dir, None, n_classes=3)
 
+    eval_config = config.EvalConfig(
+        model_specs=[config.ModelSpec(location=export_dir)])
     eval_shared_model = self.createTestEvalSharedModel(
         eval_saved_model_path=export_dir, tags=[tf.saved_model.SERVING])
-    predict_extractor = predict_extractor_v2.PredictExtractor(eval_shared_model)
+    predict_extractor = predict_extractor_v2.PredictExtractor(
+        eval_config=eval_config, eval_shared_models=[eval_shared_model])
 
     examples = [
         self._makeExample(age=1.0, language='english', label=0),
@@ -178,9 +189,12 @@ class PredictExtractorTest(testutil.TensorflowModelAnalysisTest):
     temp_export_dir = self._getExportDir()
     export_dir, _ = multi_head.simple_multi_head(temp_export_dir, None)
 
+    eval_config = config.EvalConfig(
+        model_specs=[config.ModelSpec(location=export_dir)])
     eval_shared_model = self.createTestEvalSharedModel(
         eval_saved_model_path=export_dir, tags=[tf.saved_model.SERVING])
-    predict_extractor = predict_extractor_v2.PredictExtractor(eval_shared_model)
+    predict_extractor = predict_extractor_v2.PredictExtractor(
+        eval_config=eval_config, eval_shared_models=[eval_shared_model])
 
     examples = [
         self._makeExample(
@@ -235,6 +249,78 @@ class PredictExtractorTest(testutil.TensorflowModelAnalysisTest):
 
       util.assert_that(result, check_result, label='result')
 
+  def testPredictExtractorWithMultiModels(self):
+    temp_export_dir = self._getExportDir()
+    export_dir1, _ = multi_head.simple_multi_head(temp_export_dir, None)
+    export_dir2, _ = multi_head.simple_multi_head(temp_export_dir, None)
+
+    eval_config = config.EvalConfig(model_specs=[
+        config.ModelSpec(location=export_dir1, name='model1'),
+        config.ModelSpec(location=export_dir2, name='model2')
+    ])
+    eval_shared_model1 = self.createTestEvalSharedModel(
+        eval_saved_model_path=export_dir1, tags=[tf.saved_model.SERVING])
+    eval_shared_model2 = self.createTestEvalSharedModel(
+        eval_saved_model_path=export_dir2, tags=[tf.saved_model.SERVING])
+    predict_extractor = predict_extractor_v2.PredictExtractor(
+        eval_config=eval_config,
+        eval_shared_models=[eval_shared_model1, eval_shared_model2])
+
+    examples = [
+        self._makeExample(
+            age=1.0,
+            language='english',
+            english_label=1.0,
+            chinese_label=0.0,
+            other_label=0.0),
+        self._makeExample(
+            age=1.0,
+            language='chinese',
+            english_label=0.0,
+            chinese_label=1.0,
+            other_label=0.0),
+        self._makeExample(
+            age=2.0,
+            language='english',
+            english_label=1.0,
+            chinese_label=0.0,
+            other_label=0.0),
+        self._makeExample(
+            age=2.0,
+            language='other',
+            english_label=0.0,
+            chinese_label=1.0,
+            other_label=1.0)
+    ]
+
+    with beam.Pipeline() as pipeline:
+      # pylint: disable=no-value-for-parameter
+      result = (
+          pipeline
+          | 'Create' >> beam.Create([e.SerializeToString() for e in examples])
+          | 'InputsToExtracts' >> model_eval_lib.InputsToExtracts()
+          | predict_extractor.stage_name >> predict_extractor.ptransform)
+
+      # pylint: enable=no-value-for-parameter
+
+      def check_result(got):
+        try:
+          self.assertLen(got, 4)
+          for item in got:
+            # We can't verify the actual predictions, but we can verify the keys
+            self.assertIn(constants.PREDICTIONS_KEY, item)
+            for model_name in ('model1', 'model2'):
+              self.assertIn(model_name, item[constants.PREDICTIONS_KEY])
+              for output_name in ('chinese_head', 'english_head', 'other_head'):
+                for pred_key in ('logistic', 'probabilities', 'all_classes'):
+                  self.assertIn(output_name + '/' + pred_key,
+                                item[constants.PREDICTIONS_KEY][model_name])
+
+        except AssertionError as err:
+          raise util.BeamAssertException(err)
+
+      util.assert_that(result, check_result, label='result')
+
   def testPredictExtractorWithKerasModel(self):
     input1 = tf.keras.layers.Input(shape=(1,), name='input1')
     input2 = tf.keras.layers.Input(shape=(1,), name='input2')
@@ -260,9 +346,12 @@ class PredictExtractorTest(testutil.TensorflowModelAnalysisTest):
     export_dir = self._getExportDir()
     model.save(export_dir, save_format='tf')
 
+    eval_config = config.EvalConfig(
+        model_specs=[config.ModelSpec(location=export_dir)])
     eval_shared_model = self.createTestEvalSharedModel(
         eval_saved_model_path=export_dir, tags=[tf.saved_model.SERVING])
-    predict_extractor = predict_extractor_v2.PredictExtractor(eval_shared_model)
+    predict_extractor = predict_extractor_v2.PredictExtractor(
+        eval_config=eval_config, eval_shared_models=[eval_shared_model])
 
     predict_features = [
         {
