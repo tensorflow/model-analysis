9069cb5da802872ba7ab313f3de04e943a878143
mdreves <mdreves@google.com>
2019-10-17 22:24:18 -0700

    Add calibration histogram based on new V2 infrastructure.
    PiperOrigin-RevId: 275407345

---
+++ tensorflow_model_analysis/metrics/calibration_histogram.py
@@ -0,0 +1,234 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Calibration histogram."""
+
+from __future__ import absolute_import
+from __future__ import division
+# Standard __future__ imports
+from __future__ import print_function
+
+import bisect
+import heapq
+import itertools
+import operator
+
+import apache_beam as beam
+from tensorflow_model_analysis import config
+from tensorflow_model_analysis.metrics import metric_types
+from tensorflow_model_analysis.metrics import metric_util
+from typing import Dict, List, Optional, NamedTuple, Text
+
+CALIBRATION_HISTOGRAM_NAME = '_calibration_histogram'
+
+DEFAULT_NUM_BUCKETS = 10000
+
+Bucket = NamedTuple('Bucket', [('bucket_id', int), ('weighted_labels', float),
+                               ('weighted_predictions', float),
+                               ('weighted_examples', float)])
+
+Histogram = List[Bucket]
+
+
+def calibration_histogram(
+    num_buckets: Optional[int] = None,
+    left: Optional[float] = None,
+    right: Optional[float] = None,
+    name: Text = None,
+    eval_config: Optional[config.EvalConfig] = None,
+    model_name: Text = '',
+    output_name: Text = '',
+    sub_key: Optional[metric_types.SubKey] = None
+) -> metric_types.MetricComputations:
+  """Returns metric computations for calibration histogram.
+
+  Args:
+    num_buckets: Number of buckets to use. Note that the actual number of
+      buckets will be num_buckets + 2 to account for the edge cases.
+    left: Start of predictions interval.
+    right: End of predictions interval.
+    name: Metric name.
+    eval_config: Eval config.
+    model_name: Optional model name (if multi-model evaluation).
+    output_name: Optional output name (if multi-output model type).
+    sub_key: Optional sub key.
+
+  Returns:
+    MetricComputations for computing the histogram(s).
+  """
+  if num_buckets is None:
+    num_buckets = DEFAULT_NUM_BUCKETS
+  if left is None:
+    left = 0.0
+  if right is None:
+    right = 1.0
+  if name is None:
+    name = '{}_{}'.format(CALIBRATION_HISTOGRAM_NAME, num_buckets)
+  key = metric_types.PlotKey(
+      name=name,
+      model_name=model_name,
+      output_name=output_name,
+      sub_key=sub_key)
+  return [
+      metric_types.MetricComputation(
+          keys=[key],
+          preprocessor=None,
+          combiner=_CalibrationHistogramCombiner(
+              key=key,
+              eval_config=eval_config,
+              num_buckets=num_buckets,
+              left=left,
+              right=right))
+  ]
+
+
+class _CalibrationHistogramCombiner(beam.CombineFn):
+  """Creates histogram from labels, predictions, and example weights."""
+
+  def __init__(self, key: metric_types.PlotKey, eval_config: config.EvalConfig,
+               num_buckets: int, left: float, right: float):
+    self._eval_config = eval_config
+    self._key = key
+    self._num_buckets = num_buckets
+    self._left = left
+    self._range = right - left
+
+  def _bucket_index(self, prediction: float) -> int:
+    """Returns bucket index given prediction value. Values are truncated."""
+    bucket_index = int(
+        (prediction - self._left) / self._range * self._num_buckets) + 1
+    if bucket_index < 0:
+      return 0
+    if bucket_index >= self._num_buckets + 1:
+      return self._num_buckets + 1
+    return bucket_index
+
+  def create_accumulator(self) -> Histogram:
+    # The number of accumulator (histogram) buckets is variable and depends on
+    # the number of distinct intervals that are matched during calls to
+    # add_inputs. This allows the historam size to start small and gradually
+    # grow size during calls to merge until reaching the final histogram.
+    return []
+
+  def add_input(self, accumulator: Histogram,
+                element: metric_types.StandardMetricInputs) -> Histogram:
+    labels, predictions, example_weight = (
+        metric_util.to_label_prediction_example_weight(
+            element,
+            eval_config=self._eval_config,
+            model_name=self._key.model_name,
+            output_name=self._key.output_name,
+            sub_key=self._key.sub_key,
+            array_size=1))
+    example_weight = float(example_weight)
+    for i in range(predictions.size):
+      label = float(labels[i])
+      prediction = float(predictions[i])
+      weighted_label = label * example_weight
+      weighted_prediction = prediction * example_weight
+      bucket_index = self._bucket_index(prediction)
+      # Check if bucket exists, all bucket values are > 0, so -1 are always less
+      insert_index = bisect.bisect_left(accumulator,
+                                        Bucket(bucket_index, -1, -1, -1))
+      if (insert_index == len(accumulator) or
+          accumulator[insert_index].bucket_id != bucket_index):
+        accumulator.insert(
+            insert_index,
+            Bucket(bucket_index, weighted_label, weighted_prediction,
+                   example_weight))
+      else:
+        existing_bucket = accumulator[insert_index]
+        accumulator[insert_index] = Bucket(
+            bucket_index, existing_bucket.weighted_labels + weighted_label,
+            existing_bucket.weighted_predictions + weighted_prediction,
+            existing_bucket.weighted_examples + example_weight)
+    return accumulator
+
+  def merge_accumulators(self, accumulators: List[Histogram]) -> Histogram:
+    result = []
+    for bucket_id, buckets in itertools.groupby(
+        heapq.merge(*accumulators), key=operator.attrgetter('bucket_id')):
+      total_weighted_labels = 0.0
+      total_weighted_predictions = 0.0
+      total_weighted_examples = 0.0
+      for bucket in buckets:
+        total_weighted_labels += bucket.weighted_labels
+        total_weighted_predictions += bucket.weighted_predictions
+        total_weighted_examples += bucket.weighted_examples
+      result.append(
+          Bucket(bucket_id, total_weighted_labels, total_weighted_predictions,
+                 total_weighted_examples))
+    return result
+
+  def extract_output(
+      self, accumulator: Histogram) -> Dict[metric_types.PlotKey, Histogram]:
+    return {self._key: accumulator}
+
+
+def rebin(thresholds: List[float],
+          histogram: Histogram,
+          num_buckets: int = DEFAULT_NUM_BUCKETS,
+          left: float = 0.0,
+          right: float = 1.0) -> Histogram:
+  """Applies new thresholds to an existing calibration histogram.
+
+  Args:
+    thresholds: New thresholds to apply to the histogram. Must be in sorted
+      order, but need not be evenly spaced.
+    histogram: Existing calibration histogram.
+    num_buckets: Number of buckets in existing histogram.
+    left: Left boundary for existing histogram.
+    right: Right boundary for existing histogram.
+
+  Returns:
+    A histogram of len(thresholds) where the buckets with IDs (0, 1, 2, ...)
+    correspond to the intervals:
+      [thresholds[0], thresholds[1]), ... [thresholds[i], thresholds[i+1])
+    Any values in buckets -inf or +inf will be added to the start and end
+    thresholds respectively. Unlike the input histogram empty buckets will be
+    returned.
+  """
+  buckets = []
+  offset = 0
+  total_weighted_labels = 0.0
+  total_weighted_predictions = 0.0
+  total_weighted_examples = 0.0
+  for bucket in histogram:
+    if bucket.bucket_id == 0:
+      pred = float('-inf')
+    elif bucket.bucket_id >= num_buckets + 1:
+      pred = float('inf')
+    else:
+      pred = (bucket.bucket_id - 1) / num_buckets * (right - left) + left
+    if offset + 1 < len(thresholds) and pred >= thresholds[offset + 1]:
+      buckets.append(
+          Bucket(offset, total_weighted_labels, total_weighted_predictions,
+                 total_weighted_examples))
+      offset += 1
+      total_weighted_labels = 0.0
+      total_weighted_predictions = 0.0
+      total_weighted_examples = 0.0
+      while offset + 1 < len(thresholds) and pred >= thresholds[offset + 1]:
+        buckets.append(Bucket(offset, 0.0, 0.0, 0.0))
+        offset += 1
+    total_weighted_labels += bucket.weighted_labels
+    total_weighted_predictions += bucket.weighted_predictions
+    total_weighted_examples += bucket.weighted_examples
+  buckets.append(
+      Bucket(offset, total_weighted_labels, total_weighted_predictions,
+             total_weighted_examples))
+  offset += 1
+  while offset < len(thresholds):
+    buckets.append(Bucket(offset, 0.0, 0.0, 0.0))
+    offset += 1
+  return buckets
---
+++ tensorflow_model_analysis/metrics/calibration_histogram_test.py
@@ -0,0 +1,403 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for calibration histogram."""
+
+from __future__ import absolute_import
+from __future__ import division
+# Standard __future__ imports
+from __future__ import print_function
+
+import apache_beam as beam
+from apache_beam.testing import util
+import numpy as np
+import tensorflow as tf
+from tensorflow_model_analysis.eval_saved_model import testutil
+from tensorflow_model_analysis.metrics import calibration_histogram
+from tensorflow_model_analysis.metrics import metric_types
+from tensorflow_model_analysis.metrics import metric_util
+
+
+class CalibrationHistogramTest(testutil.TensorflowModelAnalysisTest):
+
+  def testCalibrationHistogram(self):
+    histogram = calibration_histogram.calibration_histogram()[0]
+
+    example1 = {
+        'labels': np.array([0.0]),
+        'predictions': np.array([0.2]),
+        'example_weights': np.array([1.0])
+    }
+    example2 = {
+        'labels': np.array([1.0]),
+        'predictions': np.array([0.8]),
+        'example_weights': np.array([2.0])
+    }
+    example3 = {
+        'labels': np.array([0.0]),
+        'predictions': np.array([0.5]),
+        'example_weights': np.array([3.0])
+    }
+    example4 = {
+        'labels': np.array([1.0]),
+        'predictions': np.array([-0.1]),
+        'example_weights': np.array([4.0])
+    }
+    example5 = {
+        'labels': np.array([1.0]),
+        'predictions': np.array([0.5]),
+        'example_weights': np.array([5.0])
+    }
+    example6 = {
+        'labels': np.array([1.0]),
+        'predictions': np.array([0.8]),
+        'example_weights': np.array([6.0])
+    }
+    example7 = {
+        'labels': np.array([0.0]),
+        'predictions': np.array([0.2]),
+        'example_weights': np.array([7.0])
+    }
+    example8 = {
+        'labels': np.array([1.0]),
+        'predictions': np.array([1.1]),
+        'example_weights': np.array([8.0])
+    }
+
+    with beam.Pipeline() as pipeline:
+      # pylint: disable=no-value-for-parameter
+      result = (
+          pipeline
+          | 'Create' >> beam.Create([
+              example1, example2, example3, example4, example5, example6,
+              example7, example8
+          ])
+          | 'Process' >> beam.Map(metric_util.to_standard_metric_inputs)
+          | 'AddSlice' >> beam.Map(lambda x: ((), x))
+          | 'ComputeHistogram' >> beam.CombinePerKey(histogram.combiner))
+
+      # pylint: enable=no-value-for-parameter
+
+      def check_result(got):
+        try:
+          self.assertLen(got, 1)
+          got_slice_key, got_plots = got[0]
+          self.assertEqual(got_slice_key, ())
+          self.assertLen(got_plots, 1)
+          key = metric_types.PlotKey('_calibration_histogram_10000')
+          self.assertIn(key, got_plots)
+          got_histogram = got_plots[key]
+          self.assertLen(got_histogram, 5)
+          self.assertEqual(
+              got_histogram[0],
+              calibration_histogram.Bucket(
+                  bucket_id=0,
+                  weighted_labels=1.0 * 4.0,
+                  weighted_predictions=-0.1 * 4.0,
+                  weighted_examples=4.0))
+          self.assertEqual(
+              got_histogram[1],
+              calibration_histogram.Bucket(
+                  bucket_id=2001,
+                  weighted_labels=0.0 + 0.0,
+                  weighted_predictions=0.2 + 7 * 0.2,
+                  weighted_examples=1.0 + 7.0))
+          self.assertEqual(
+              got_histogram[2],
+              calibration_histogram.Bucket(
+                  bucket_id=5001,
+                  weighted_labels=1.0 * 5.0,
+                  weighted_predictions=0.5 * 3.0 + 0.5 * 5.0,
+                  weighted_examples=3.0 + 5.0))
+          self.assertEqual(
+              got_histogram[3],
+              calibration_histogram.Bucket(
+                  bucket_id=8001,
+                  weighted_labels=1.0 * 2.0 + 1.0 * 6.0,
+                  weighted_predictions=0.8 * 2.0 + 0.8 * 6.0,
+                  weighted_examples=2.0 + 6.0))
+          self.assertEqual(
+              got_histogram[4],
+              calibration_histogram.Bucket(
+                  bucket_id=10001,
+                  weighted_labels=1.0 * 8.0,
+                  weighted_predictions=1.1 * 8.0,
+                  weighted_examples=8.0))
+
+        except AssertionError as err:
+          raise util.BeamAssertException(err)
+
+      util.assert_that(result, check_result, label='result')
+
+  def testCalibrationHistogramWithK(self):
+    histogram = calibration_histogram.calibration_histogram(
+        sub_key=metric_types.SubKey(k=2))[0]
+
+    example1 = {
+        'labels': np.array([2]),
+        'predictions': np.array([0.2, 0.05, 0.1, 0.05]),
+        'example_weights': np.array([1.0])
+    }
+    example2 = {
+        'labels': np.array([2]),
+        'predictions': np.array([0.7, 0.1, 0.8, 0.5]),
+        'example_weights': np.array([2.0])
+    }
+    example3 = {
+        'labels': np.array([3]),
+        'predictions': np.array([0.1, 0.5, 0.3, 0.4]),
+        'example_weights': np.array([3.0])
+    }
+    example4 = {
+        'labels': np.array([0]),
+        'predictions': np.array([-0.1, -0.2, -0.7, -0.4]),
+        'example_weights': np.array([4.0])
+    }
+    example5 = {
+        'labels': np.array([1]),
+        'predictions': np.array([0.3, 0.5, 0.0, 0.4]),
+        'example_weights': np.array([5.0])
+    }
+    example6 = {
+        'labels': np.array([2]),
+        'predictions': np.array([0.1, 0.1, 0.8, 0.7]),
+        'example_weights': np.array([6.0])
+    }
+    example7 = {
+        'labels': np.array([2]),
+        'predictions': np.array([0.0, 0.2, 0.1, 0.0]),
+        'example_weights': np.array([7.0])
+    }
+    example8 = {
+        'labels': np.array([0]),
+        'predictions': np.array([1.1, 0.3, 1.05, 0.2]),
+        'example_weights': np.array([8.0])
+    }
+
+    with beam.Pipeline() as pipeline:
+      # pylint: disable=no-value-for-parameter
+      result = (
+          pipeline
+          | 'Create' >> beam.Create([
+              example1, example2, example3, example4, example5, example6,
+              example7, example8
+          ])
+          | 'Process' >> beam.Map(metric_util.to_standard_metric_inputs)
+          | 'AddSlice' >> beam.Map(lambda x: ((), x))
+          | 'ComputeHistogram' >> beam.CombinePerKey(histogram.combiner))
+
+      # pylint: enable=no-value-for-parameter
+
+      def check_result(got):
+        try:
+          self.assertLen(got, 1)
+          got_slice_key, got_plots = got[0]
+          self.assertEqual(got_slice_key, ())
+          self.assertLen(got_plots, 1)
+          key = metric_types.PlotKey(
+              name='_calibration_histogram_10000',
+              sub_key=metric_types.SubKey(k=2))
+          self.assertIn(key, got_plots)
+          got_histogram = got_plots[key]
+          self.assertLen(got_histogram, 5)
+          self.assertEqual(
+              got_histogram[0],
+              calibration_histogram.Bucket(
+                  bucket_id=0,
+                  weighted_labels=0.0 * 4.0,
+                  weighted_predictions=-0.2 * 4.0,
+                  weighted_examples=4.0))
+          self.assertEqual(
+              got_histogram[1],
+              calibration_histogram.Bucket(
+                  bucket_id=1001,
+                  weighted_labels=1.0 + 7 * 1.0,
+                  weighted_predictions=0.1 + 7 * 0.1,
+                  weighted_examples=1.0 + 7.0))
+          self.assertEqual(
+              got_histogram[2],
+              calibration_histogram.Bucket(
+                  bucket_id=4001,
+                  weighted_labels=1.0 * 3.0 + 0.0 * 5.0,
+                  weighted_predictions=0.4 * 3.0 + 0.4 * 5.0,
+                  weighted_examples=3.0 + 5.0))
+          self.assertEqual(
+              got_histogram[3],
+              calibration_histogram.Bucket(
+                  bucket_id=7001,
+                  weighted_labels=0.0 * 2.0 + 0.0 * 6.0,
+                  weighted_predictions=0.7 * 2.0 + 0.7 * 6.0,
+                  weighted_examples=2.0 + 6.0))
+          self.assertEqual(
+              got_histogram[4],
+              calibration_histogram.Bucket(
+                  bucket_id=10001,
+                  weighted_labels=0.0 * 8.0,
+                  weighted_predictions=1.05 * 8.0,
+                  weighted_examples=8.0))
+
+        except AssertionError as err:
+          raise util.BeamAssertException(err)
+
+      util.assert_that(result, check_result, label='result')
+
+  def testTopKCalibrationHistogramWithTopK(self):
+    histogram = calibration_histogram.calibration_histogram(
+        sub_key=metric_types.SubKey(top_k=2))[0]
+
+    example1 = {
+        'labels': np.array([2]),
+        'predictions': np.array([0.2, 0.05, 0.5, 0.05]),
+        'example_weights': np.array([1.0])
+    }
+    example2 = {
+        'labels': np.array([2]),
+        'predictions': np.array([0.8, 0.1, 0.8, 0.5]),
+        'example_weights': np.array([2.0])
+    }
+    example3 = {
+        'labels': np.array([3]),
+        'predictions': np.array([0.2, 0.5, 0.1, 0.1]),
+        'example_weights': np.array([3.0])
+    }
+    example4 = {
+        'labels': np.array([0]),
+        'predictions': np.array([-0.1, 1.1, -0.7, -0.4]),
+        'example_weights': np.array([4.0])
+    }
+
+    with beam.Pipeline() as pipeline:
+      # pylint: disable=no-value-for-parameter
+      result = (
+          pipeline
+          | 'Create' >> beam.Create([example1, example2, example3, example4])
+          | 'Process' >> beam.Map(metric_util.to_standard_metric_inputs)
+          | 'AddSlice' >> beam.Map(lambda x: ((), x))
+          | 'ComputeHistogram' >> beam.CombinePerKey(histogram.combiner))
+
+      # pylint: enable=no-value-for-parameter
+
+      def check_result(got):
+        try:
+          self.assertLen(got, 1)
+          got_slice_key, got_plots = got[0]
+          self.assertEqual(got_slice_key, ())
+          self.assertLen(got_plots, 1)
+          key = metric_types.PlotKey(
+              name='_calibration_histogram_10000',
+              sub_key=metric_types.SubKey(top_k=2))
+          self.assertIn(key, got_plots)
+          got_histogram = got_plots[key]
+          self.assertLen(got_histogram, 5)
+          self.assertEqual(
+              got_histogram[0],
+              calibration_histogram.Bucket(
+                  bucket_id=0,
+                  weighted_labels=1.0 * 4.0,
+                  weighted_predictions=-0.1 * 4.0,
+                  weighted_examples=4.0))
+          self.assertEqual(
+              got_histogram[1],
+              calibration_histogram.Bucket(
+                  bucket_id=2001,
+                  weighted_labels=0.0 + 0.0,
+                  weighted_predictions=0.2 + 3 * 0.2,
+                  weighted_examples=1.0 + 3.0))
+          self.assertEqual(
+              got_histogram[2],
+              calibration_histogram.Bucket(
+                  bucket_id=5001,
+                  weighted_labels=1.0 + 0.0 * 3.0,
+                  weighted_predictions=0.5 * 1.0 + 0.5 * 3.0,
+                  weighted_examples=1.0 + 3.0))
+          self.assertEqual(
+              got_histogram[3],
+              calibration_histogram.Bucket(
+                  bucket_id=8001,
+                  weighted_labels=0.0 * 2.0 + 1.0 * 2.0,
+                  weighted_predictions=0.8 * 2.0 + 0.8 * 2.0,
+                  weighted_examples=2.0 + 2.0))
+          self.assertEqual(
+              got_histogram[4],
+              calibration_histogram.Bucket(
+                  bucket_id=10001,
+                  weighted_labels=0.0 * 4.0,
+                  weighted_predictions=1.1 * 4.0,
+                  weighted_examples=4.0))
+
+        except AssertionError as err:
+          raise util.BeamAssertException(err)
+
+      util.assert_that(result, check_result, label='result')
+
+  def testRebin(self):
+    # [Bucket(0, -1, -0.01), Bucket(1, 0, 0) ... Bucket(101, 101, 1.01)]
+    histogram = [calibration_histogram.Bucket(0, -1, -.01, 1.0)]
+    for i in range(100):
+      histogram.append(calibration_histogram.Bucket(i + 1, i, i * .01, 1.0))
+    histogram.append(calibration_histogram.Bucket(101, 101, 1.01, 1.0))
+    # [-1e-7, 0.0, 0.1, ..., 0.9, 1.0, 1.0+1e-7]
+    thresholds = [-1e-7] + [i * 1.0 / 10 for i in range(11)] + [1.0 + 1e-7]
+    got = calibration_histogram.rebin(thresholds, histogram, 100)
+
+    # labels = (10 * (i-1)) + (1 + 2 + 3 + ... + 9)
+    expected = [
+        calibration_histogram.Bucket(0, -1, -0.01, 1.0),
+        calibration_histogram.Bucket(1, 45.0, 0.45, 10.0),
+        calibration_histogram.Bucket(2, 145.0, 1.45, 10.0),
+        calibration_histogram.Bucket(3, 245.0, 2.45, 10.0),
+        calibration_histogram.Bucket(4, 345.0, 3.45, 10.0),
+        calibration_histogram.Bucket(5, 445.0, 4.45, 10.0),
+        calibration_histogram.Bucket(6, 545.0, 5.45, 10.0),
+        calibration_histogram.Bucket(7, 645.0, 6.45, 10.0),
+        calibration_histogram.Bucket(8, 745.0, 7.45, 10.0),
+        calibration_histogram.Bucket(9, 845.0, 8.45, 10.0),
+        calibration_histogram.Bucket(10, 945.0, 9.45, 10.0),
+        calibration_histogram.Bucket(11, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(12, 101.0, 1.01, 1.0),
+    ]
+    self.assertLen(got, len(expected))
+    for i in range(len(got)):
+      self.assertSequenceAlmostEqual(got[i], expected[i])
+
+  def testRebinWithSparseData(self):
+    histogram = [
+        calibration_histogram.Bucket(4, 5.0, .25, 5.0),  # pred = .05
+        calibration_histogram.Bucket(61, 60.0, 36.0, 60.0),  # pred = .6
+        calibration_histogram.Bucket(70, 69.0, 47.61, 69.0),  # pred = .69
+        calibration_histogram.Bucket(100, 99.0, 98.01, 99.0)  # pred = .99
+    ]
+    # [0, 0.1, ..., 0.9, 1.0]
+    thresholds = [i * 1.0 / 10 for i in range(0, 11)]
+    got = calibration_histogram.rebin(thresholds, histogram, 100)
+
+    expected = [
+        calibration_histogram.Bucket(0, 5.0, 0.25, 5.0),
+        calibration_histogram.Bucket(1, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(2, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(3, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(4, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(5, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(6, 129.0, 83.61, 129.0),
+        calibration_histogram.Bucket(7, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(8, 0.0, 0.0, 0.0),
+        calibration_histogram.Bucket(9, 99.0, 98.01, 99.0),
+        calibration_histogram.Bucket(10, 0.0, 0.0, 0.0),
+    ]
+    self.assertLen(got, len(expected))
+    for i in range(len(got)):
+      self.assertSequenceAlmostEqual(got[i], expected[i])
+
+
+if __name__ == '__main__':
+  tf.test.main()
