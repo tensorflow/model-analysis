Fix the standard error calculation in TFMA Poisson bootstrap.
PiperOrigin-RevId: 280760559

tensorflow_model_analysis/math_util.py
@@ -17,7 +17,6 @@ from __future__ import absolute_import
 from __future__ import division
 # Standard __future__ imports
 
-import numpy as np
 from scipy import stats
 from tensorflow_model_analysis import types
 
@@ -26,8 +25,7 @@ def calculate_confidence_interval(
     t_distribution_value: types.ValueWithTDistribution):
   """Caculate confidence intervals based 95% confidence level."""
   alpha = 0.05
-  std_err = t_distribution_value.sample_standard_deviation / np.sqrt(
-      t_distribution_value.sample_degrees_of_freedom + 1)
+  std_err = t_distribution_value.sample_standard_deviation
   t_stat = stats.t.ppf(1 - (alpha / 2.0),
                        t_distribution_value.sample_degrees_of_freedom)
   upper_bound = t_distribution_value.sample_mean + t_stat * std_err

tensorflow_model_analysis/math_util_test.py
@@ -18,6 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 import math
+import numpy as np
 import tensorflow as tf
 from tensorflow_model_analysis import math_util
 from tensorflow_model_analysis import types
@@ -26,10 +27,10 @@ from tensorflow_model_analysis import types
 class MathUtilTest(tf.test.TestCase):
 
   def testCalculateConfidenceInterval(self):
-    self.assertEqual(
+    np.testing.assert_almost_equal(
         math_util.calculate_confidence_interval(
             types.ValueWithTDistribution(10, 2, 9, 10)),
-        (10, 8.5692861880948552, 11.430713811905145))
+        (10, 5.4756856744035902196, 14.524314325596410669))
     mean, lb, ub = math_util.calculate_confidence_interval(
         types.ValueWithTDistribution(-1, -1, -1, -1))
     self.assertEqual(mean, -1)