6e27187d75746f705dd126b2d668beb93011e72c
tf-model-analysis-team <tensorflow-extended-team@google.com>
2019-12-19 17:07:05 -0800

    internal change
    PiperOrigin-RevId: 286492011

--- tensorflow_model_analysis/addons/fairness/notebook/visualization.py
+++ tensorflow_model_analysis/addons/fairness/notebook/visualization.py
@@ -23,4 +23,4 @@ if _is_colab():
   from tensorflow_model_analysis.addons.fairness.notebook.colab.renderer import *  # pylint: disable=wildcard-import,g-import-not-at-top
   from tensorflow_model_analysis.addons.fairness.notebook.colab.widget import *  # pylint: disable=wildcard-import,g-import-not-at-top
 else:
-  pass
+  from tensorflow_model_analysis.addons.fairness.notebook.jupyter.widget import *  # pylint: disable=wildcard-import,g-import-not-at-top
