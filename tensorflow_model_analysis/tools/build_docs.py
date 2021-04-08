ae8e4b163b1d88cd7865a58f887f65cadc013304
yashkatariya <yashkatariya@google.com>
2021-04-08 15:35:10 -0700

    Add the gen_report flag
    PiperOrigin-RevId: 367520518

--- tensorflow_model_analysis/tools/build_docs.py
+++ tensorflow_model_analysis/tools/build_docs.py
@@ -57,6 +57,10 @@ flags.DEFINE_bool('search_hints', True,
 flags.DEFINE_string('site_path', 'tfx/model_analysis/api_docs/python',
                     'Path prefix in the _toc.yaml')
 
+flags.DEFINE_bool('gen_report', False,
+                  ('Generate an API report containing the health of the'
+                   'docstrings of the public API.'))
+
 FLAGS = flags.FLAGS
 
 # pylint: disable=line-too-long
@@ -184,6 +188,7 @@ def main(args):
       search_hints=FLAGS.search_hints,
       site_path=FLAGS.site_path,
       private_map={},
+      gen_report=FLAGS.gen_report,
       callbacks=[
           public_api.local_definitions_filter, depth_filter, suppress_docs
       ])
