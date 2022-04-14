aad2688e39d144ffd2aa4021a54103d4271e226c
gorijala <gorijala@google.com>
2022-04-14 13:17:35 -0700

    Updating Setup.py and README.md files with support py39.
    PiperOrigin-RevId: 441837818

--- setup.py
+++ setup.py
@@ -337,6 +337,7 @@ setup_args = {
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
