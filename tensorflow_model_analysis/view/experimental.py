b499acfa894505aac2ee1ec2ff6fe0d6c1bed7b4
jinhuang <jinhuang@google.com>
2022-05-06 14:56:37 -0700

    Adds a util to convert Iterable of MetricsForSlice to Dataframe.
    PiperOrigin-RevId: 447077351

--- tensorflow_model_analysis/view/experimental.py
+++ tensorflow_model_analysis/view/experimental.py
@@ -14,7 +14,7 @@
 """Experimental output format and util for Tensorflow Model Analysis."""
 
 import collections
-from typing import Any, Iterator, Optional, Tuple, Union
+from typing import Any, Iterable, Iterator, Optional, Tuple, Union
 
 import pandas as pd
 from tensorflow_model_analysis.api import model_eval_lib
@@ -23,7 +23,7 @@ from tensorflow_model_analysis.proto import metrics_for_slice_pb2
 
 # Dataframe output columns.
 _DISPLAY_VALUE = 'display_value'
-_METRIC_AVLUE = 'metric_value'
+_METRIC_VALUE = 'metric_value'
 _CONFIDENCE_INTERVAL = 'confidence_interval'
 _SLICE = 'slice'
 
@@ -62,6 +62,67 @@ def _slice_key_str(slice_key: metrics_for_slice_pb2.SliceKey) -> str:
       for single_slice_key in slice_key.single_slice_keys)
 
 
+def metrics_as_dataframe(
+    metrics_for_slices: Iterable[metrics_for_slice_pb2.MetricsForSlice],
+    include_empty_columns: bool = False,
+) -> pd.DataFrame:
+  """Convert the deserialized MetricForSlice protos to Pandas dataframe.
+
+  Args:
+    metrics_for_slices: an interable of MetricForSlice proto.
+    include_empty_columns: include a column if its value is not empty (None) in
+      corresponding field in the MetricKey.
+
+  Returns:
+    A dataframe with the following columns if the value is not None:
+    * slice: the string representation of the slice, e.g., "age=10;sex=male".
+    * recursively flattened items in metric_key: name, model_name, output_name,
+      is_diff, example_weighted, flattened sub_key (class_id, k, top_k),
+      flattened aggregation_type (micro_average, macro_average,
+      weighted_macro_average).
+    * metric_value: a tfma.metrics.MetricValue proto.
+    * confidence_interval: a tfma.metrics.ConfidenceInterval proto.
+    * display_value: a best effort string reprensentation of the underlying
+      metric value and confidence interval if turned on.
+  """
+
+  def _get_display_value(val: metrics_for_slice_pb2.MetricValue) -> Any:
+    # Best effort to convert the metric value to a concise representation.
+    value_type = val.WhichOneof('type')
+    if value_type == 'double_value':
+      return str(val.double_value.value)
+    # TODO(b/230781704): support other types of metric values.
+    else:
+      return str(val)
+
+  # Create a pd series per column for the dataframe.
+  columns = collections.defaultdict(list)
+  index = 0
+  for metrics_for_slice in metrics_for_slices:
+    slice_str = _slice_key_str(metrics_for_slice.slice_key)
+    for key_and_value in metrics_for_slice.metric_keys_and_values:
+      columns[_SLICE].append((slice_str, index))
+      for k, v in _flatten_namedtuple(
+          metric_types.MetricKey.from_proto(key_and_value.key),
+          include_empty_columns):
+        columns[k].append((v, index))
+      columns[_DISPLAY_VALUE].append(
+          (_get_display_value(key_and_value.value), index))
+      columns[_METRIC_VALUE].append((key_and_value.value, index))
+      if include_empty_columns:
+        if key_and_value.HasField(_CONFIDENCE_INTERVAL):
+          columns[_CONFIDENCE_INTERVAL].append(
+              (key_and_value.confidence_interval, index))
+        else:
+          columns[_CONFIDENCE_INTERVAL].append((None, index))
+      index += 1
+
+  return pd.DataFrame({
+      column_name: pd.Series(*zip(*values))
+      for column_name, values in columns.items()
+  })
+
+
 def load_metrics_as_dataframe(
     output_path: str,
     output_file_format: str = 'tfrecord',
@@ -98,39 +159,7 @@ def load_metrics_as_dataframe(
       metric value and confidence interval if turned on.
   """
 
-  def _get_display_value(val: metrics_for_slice_pb2.MetricValue) -> Any:
-    # Best effort to convert the metric value to a concise representation.
-    value_type = val.WhichOneof('type')
-    if value_type == 'double_value':
-      return str(val.double_value.value)
-    # TODO(b/230781704): support other types of metric values.
-    else:
-      return str(val)
-
-  # Create a pd series per column for the dataframe.
-  columns = collections.defaultdict(list)
-  index = 0
-  for metrics_for_slice in model_eval_lib.load_metrics(output_path,
-                                                       output_file_format):
-    slice_str = _slice_key_str(metrics_for_slice.slice_key)
-    for key_and_value in metrics_for_slice.metric_keys_and_values:
-      columns[_SLICE].append((slice_str, index))
-      for k, v in _flatten_namedtuple(
-          metric_types.MetricKey.from_proto(key_and_value.key),
-          include_empty_columns):
-        columns[k].append((v, index))
-      columns[_DISPLAY_VALUE].append(
-          (_get_display_value(key_and_value.value), index))
-      columns[_METRIC_AVLUE].append((key_and_value.value, index))
-      if include_empty_columns:
-        if key_and_value.HasField(_CONFIDENCE_INTERVAL):
-          columns[_CONFIDENCE_INTERVAL].append(
-              (key_and_value.confidence_interval, index))
-        else:
-          columns[_CONFIDENCE_INTERVAL].append((None, index))
-      index += 1
-
-  return pd.DataFrame({
-      column_name: pd.Series(*zip(*values))
-      for column_name, values in columns.items()
-  })
+  return metrics_as_dataframe(
+      metrics_for_slices=model_eval_lib.load_metrics(output_path,
+                                                     output_file_format),
+      include_empty_columns=include_empty_columns)
