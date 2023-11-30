9e342d4ffda9ec8d4b9236c306467a86c6ad6cca
jinhuang <jinhuang@google.com>
2023-11-30 12:59:30 -0800

    Support multi-class encoded input for confusion matrix aggregate.
    PiperOrigin-RevId: 586761925

--- tensorflow_model_analysis/experimental/metrics/aggregates/_classification.py
+++ tensorflow_model_analysis/experimental/metrics/aggregates/_classification.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 """Aggregates modules for all classification metrics."""
 
+from collections.abc import Sequence
 import dataclasses
 import enum
+import itertools
 from typing import Any
 
 import numpy as np
@@ -24,8 +26,7 @@ from tensorflow_model_analysis.experimental.lazytf import api as lazytf
 _NumbersT = npt.ArrayLike
 
 
-@dataclasses.dataclass
-class ConfusionMatrixAcc:
+class ConfusionMatrix:
   """Confusion Matrix accumulator with kwargs used to compute it.
 
   Confusion matrix for classification and retrieval tasks.
@@ -36,16 +37,24 @@ class ConfusionMatrixAcc:
     tn: True negatives count.
     fp: False positives count.
     fn: False negative count.
-    config: The relevant configuration for downstream calculations, e.g, micro
-      averaged confusion matrix is computed differently from macro averaged
-      confusion matrix for precision, recall.
+    dtype: data type of the instance numpy array. None by default, dtype is
+      deduced by numpy at construction.
   """
 
-  tp: _NumbersT
-  tn: _NumbersT
-  fp: _NumbersT
-  fn: _NumbersT
-  config: dict[str, Any] = dataclasses.field(default_factory=dict)
+  def __init__(
+      self,
+      tp: _NumbersT,
+      tn: _NumbersT,
+      fp: _NumbersT,
+      fn: _NumbersT,
+      *,
+      dtype: type[Any] | None = None,
+  ):
+    self.tp = np.asarray(tp, dtype=dtype)
+    self.tn = np.asarray(tn, dtype=dtype)
+    self.fp = np.asarray(fp, dtype=dtype)
+    self.fn = np.asarray(fn, dtype=dtype)
+    self.dtype = dtype
 
   @property
   def t(self):
@@ -57,14 +66,7 @@ class ConfusionMatrixAcc:
     """Predicted True count."""
     return self.tp + self.fp
 
-  def _verify_config(self, other):
-    if self.config != other.config:
-      raise ValueError(
-          f'Incompatile config: {other.config}: \nOriginal:{self.config}'
-      )
-
   def __iadd__(self, other):
-    self._verify_config(other)
     self.tp += other.tp
     self.tn += other.tn
     self.fp += other.fp
@@ -72,21 +74,29 @@ class ConfusionMatrixAcc:
     return self
 
   def __add__(self, other):
-    self._verify_config(other)
     tp = self.tp + other.tp
     tn = self.tn + other.tn
     fp = self.fp + other.fp
     fn = self.fn + other.fn
-    return ConfusionMatrixAcc(tp, tn, fp, fn, self.config)
+    return ConfusionMatrix(tp, tn, fp, fn)
 
   def __eq__(self, other):
-    """Numerically equals ignoring config."""
+    """Numerically equals."""
     return (
         np.allclose(self.tp, other.tp)
         and np.allclose(self.tn, other.tn)
         and np.allclose(self.fp, other.fp)
         and np.allclose(self.fn, other.fn)
     )
+
+  @classmethod
+  def concatenate(cls, cms: Sequence['ConfusionMatrix']):
+    """Helper function to concatenate each nparray inside a ConfusionMatrix."""
+    tp = np.concatenate([cm.tp for cm in cms], axis=0)
+    tn = np.concatenate([cm.tn for cm in cms], axis=0)
+    fp = np.concatenate([cm.fp for cm in cms], axis=0)
+    fn = np.concatenate([cm.fn for cm in cms], axis=0)
+    return cls(tp, tn, fp, fn)
 
 
 # TODO(b/312290886): move this to Python StrEnum when moved to Python 3.11.
@@ -135,15 +145,15 @@ class AverageType(_StrEnum):
   BINARY = 'binary'
 
 
-def confusion_matrix(
-    y_true: npt.ArrayLike,
-    y_pred: npt.ArrayLike,
+def _indicator_confusion_matrix(
+    y_true: _NumbersT,
+    y_pred: _NumbersT,
     *,
     pos_label=1,
-    input_type: _StrEnum = InputType.BINARY,
-    average: _StrEnum = AverageType.MICRO,
+    multiclass: bool = False,
+    average: AverageType = AverageType.MICRO,
     unknown_tn: bool = False,
-) -> ConfusionMatrixAcc:
+) -> ConfusionMatrix:
   """Calculates confusion matix.
 
   Args:
@@ -153,7 +163,8 @@ def confusion_matrix(
       `input_type`.
     pos_label: label to be recognized as positive class, only relevant when
       input type is "binary".
-    input_type: encoding types of the y_true and y_pred.
+    multiclass: If True, input is encoded as 2D array-like of "multi-hot"
+      encoding in a shape of Batch X NumClass.
     average: average type of the confusion matrix.
     unknown_tn: tn can be unknown if the total number of classes cannot be
       deduced from the inputs shape, this is most useful for the
@@ -162,10 +173,6 @@ def confusion_matrix(
   Returns:
     Confusion matrix.
   """
-  # TODO(b/311208939): Implement other types of input type.
-  if input_type not in (InputType.BINARY, InputType.MULTICLASS_INDICATOR):
-    raise NotImplementedError(f'{input_type=} is not supported.')
-
   if average in (AverageType.MICRO, AverageType.BINARY):
     axis = None
   elif average == AverageType.MACRO:
@@ -175,12 +182,28 @@ def confusion_matrix(
   else:
     raise NotImplementedError(f'{average=} is not supported.')
 
-  y_true, y_pred = np.asanyarray(y_true), np.asarray(y_pred)
+  y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+  if multiclass and (y_true.ndim < 2 or y_pred.ndim < 2):
+    raise ValueError(
+        'Multiclass indicator input needs to be 2D array, actual dimensions:'
+        f' y_true: {y_true.ndim}; y_pred: {y_pred.ndim}'
+    )
+
   true = y_true == pos_label
   positive = y_pred == pos_label
-  if input_type == InputType.BINARY and average != AverageType.BINARY:
-    positive = np.vstack((positive, ~positive)).T
+  # Forces the input to binary if average type is binary.
+  if multiclass and average == AverageType.BINARY:
+    if true.shape[1] > 2:
+      raise ValueError(
+          'Non-binary multiclass indicator input is not supported for `binary`'
+          f' average. #classes is {true.shape[1]}'
+      )
+    true = true[:, 0]
+    positive = positive[:, 0]
+  # Reshapes to a multi-class format if average type is not BINARY.
+  elif not multiclass and average != AverageType.BINARY:
     true = np.vstack((true, ~true)).T
+    positive = np.vstack((positive, ~positive)).T
   negative = ~positive
   tp = positive & true
   fn = negative & true
@@ -194,36 +217,151 @@ def confusion_matrix(
   else:
     negative_cnt = negative.sum(axis=axis)
     tn_cnt = negative_cnt - fn_cnt
-  return ConfusionMatrixAcc(
-      tp_cnt,
-      tn_cnt,
-      fp_cnt,
-      fn_cnt,
-      config=dict(average=average),
+  return ConfusionMatrix(tp_cnt, tn_cnt, fp_cnt, fn_cnt)
+
+
+def _get_vocab(rows: Sequence[Any], multioutput: bool):
+  """Constructs a vocabulary that maps hashables to an integer."""
+  if multioutput:
+    return {
+        k: i for i, k in enumerate(set(itertools.chain.from_iterable(rows)))
+    }
+  else:
+    return {k: i for i, k in enumerate(set(rows))}
+
+
+def _apply_vocab(rows: Sequence[Any], vocab: dict[str, int], multioutput: bool):
+  """Given a vocabulary, converts a multioutput input to a indicator output."""
+  result = np.empty((len(rows), len(vocab)), dtype=np.bool_)
+  result.fill(False)
+  if multioutput:
+    for i, row in enumerate(rows):
+      for elem in row:
+        result[i][vocab[elem]] = True
+  else:
+    for i, elem in enumerate(rows):
+      result[i][vocab[elem]] = True
+  return result
+
+
+def _multiclass_confusion_matrix(
+    y_true: _NumbersT,
+    y_pred: _NumbersT,
+    *,
+    vocab: dict[str, int] | None = None,
+    multioutput: bool = False,
+    average: AverageType = AverageType.MICRO,
+    unknown_tn: bool = True,
+) -> ConfusionMatrix:
+  """Calculates a confusion matrix for multiclass(-multioutput) input.
+
+  Args:
+    y_true: ground truth classification labels. This has to be encoded in one of
+      the `multiclass` or `multiclass-output`.
+    y_pred: classification preditions. This has to be encoded in one of the
+      `multiclass` or `multiclass-output`.
+    vocab: an external vocabulary that maps categorical value to integer class
+      id. If not provided, one is deduced within this input.
+    multioutput: encoding types of the y_true and y_pred, if True, the input is
+      a nested list of class identifiers.
+    average: average type of the confusion matrix.
+    unknown_tn: tn can be unknown if the total number of classes cannot be
+      deduced from the inputs shape, this is most useful for the
+      multiclass-multioutput case where only the trues and positives are known.
+
+  Returns:
+    Confusion matrices with k in k_list.
+  """
+  vocab = vocab or (
+      _get_vocab(y_true, multioutput) | _get_vocab(y_pred, multioutput)
+  )
+  y_true_dense = _apply_vocab(y_true, vocab, multioutput)
+  y_pred_dense = _apply_vocab(y_pred, vocab, multioutput)
+  return _indicator_confusion_matrix(
+      y_true_dense,
+      y_pred_dense,
+      average=average,
+      multiclass=True,
+      pos_label=True,
+      unknown_tn=unknown_tn,
   )
 
 
-@dataclasses.dataclass(kw_only=True, slots=True)
-class ConfusionMatrix(lazytf.AggregateFn):
-  """ConfusionMatrix aggregate."""
+@dataclasses.dataclass(kw_only=True)
+class ConfusionMatrixAggregate(lazytf.AggregateFn):
+  """ConfusionMatrix aggregate.
 
-  pos_label = 1
-  input_type: _StrEnum = InputType.BINARY
-  average: _StrEnum = AverageType.BINARY
+  Attributes:
+    pos_label: the value considered as positive, default to 1.
+    input_type: input encoding type, must be one of `InputType`.
+    average: average type, must be one of `AverageType`.
+    unkown_tn: unkonw true negative, applicable when the number of classes is
+      unknown, typically used for retrieval cases. If True, the true negative is
+      set to inf.
+    vocab: an external vocabulary that maps categorical value to integer class
+      id. This is required if computed distributed (when merge_accumulators is
+      called) and the average is macro where the class id mapping needs to be
+      stable.
+  """
+
+  pos_label: bool | int | str | bytes = 1
+  input_type: str | InputType = InputType.BINARY
+  # TODO(b/311208939): implements average = None.
+  average: str | AverageType = AverageType.BINARY
   unknown_tn: bool = False
+  vocab: dict[str, int] | None = None
 
-  def add_inputs(self, accumulator: Any, *inputs: Any, **kwargs: Any) -> Any:
-    # Override by call signature.
-    config = dataclasses.asdict(self)
-    config.update(kwargs)
-    result = confusion_matrix(*inputs, **config)
+  def _calculate_confusion_matrix(
+      self,
+      y_true: _NumbersT,
+      y_pred: _NumbersT,
+  ) -> ConfusionMatrix:
+    input_type = InputType(self.input_type)
+    if input_type in (InputType.MULTICLASS_INDICATOR, InputType.BINARY):
+      return _indicator_confusion_matrix(
+          y_true,
+          y_pred,
+          pos_label=self.pos_label,
+          multiclass=(input_type == InputType.MULTICLASS_INDICATOR),
+          average=AverageType(self.average),
+          unknown_tn=self.unknown_tn,
+      )
+    elif input_type in (
+        InputType.MULTICLASS,
+        InputType.MULTICLASS_MULTIOUTPUT,
+    ):
+      return _multiclass_confusion_matrix(
+          y_true,
+          y_pred,
+          vocab=self.vocab,
+          multioutput=(input_type == InputType.MULTICLASS_MULTIOUTPUT),
+          average=self.average,
+          unknown_tn=self.unknown_tn,
+      )
+    else:
+      # TODO(b/311208939): implements continuous input type with thresholds.
+      raise NotImplementedError(f'{input_type=} is not supported.')
+
+  def add_inputs(self, accumulator: Any, *inputs: Any) -> Any:
+    result = self._calculate_confusion_matrix(*inputs)
     if accumulator:
-      result += accumulator
+      # TODO(b/311208939): implements distributed efficient samplewise metric.
+      if self.average == AverageType.SAMPLES:
+        result = result.__class__.concatenate((accumulator, result))
+      else:
+        result += accumulator
     return result
 
   def merge_accumulators(
-      self, accumulators: list[ConfusionMatrixAcc]
-  ) -> ConfusionMatrixAcc:
+      self, accumulators: list[ConfusionMatrix]
+  ) -> ConfusionMatrix:
+    if self.average == AverageType.SAMPLES:
+      return accumulators[0].__class__.concatenate(accumulators)
+    if (
+        self.average in (AverageType.WEIGHTED, AverageType.MACRO)
+        and self.vocab is None
+    ):
+      raise ValueError(f'Global vocab is needed for {self.average} average.')
     iter_acc = iter(accumulators)
     result = next(iter_acc)
     for accumulator in iter_acc:
--- tensorflow_model_analysis/experimental/metrics/aggregates/_classification_test.py
+++ tensorflow_model_analysis/experimental/metrics/aggregates/_classification_test.py
@@ -25,9 +25,24 @@ class ClassificationTest(parameterized.TestCase):
           y_true=[1, 1, 0, 0, 1, 0, 1],
           input_type="binary",
           average="binary",
-          expected=_classification.ConfusionMatrixAcc(
-              tp=2, tn=2, fp=1, fn=2, config=dict(average="binary")
-          ),
+          expected=_classification.ConfusionMatrix(tp=2, tn=2, fp=1, fn=2),
+      ),
+      dict(
+          testcase_name="multiclass_indicator_binary_average",
+          y_pred=[[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1], [0, 1]],
+          y_true=[[1, 0], [1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0]],
+          input_type="multiclass-indicator",
+          average="binary",
+          expected=_classification.ConfusionMatrix(tp=2, tn=2, fp=1, fn=2),
+      ),
+      dict(
+          testcase_name="binary_string_binary_average",
+          y_pred=["Y", "N", "Y", "N", "Y", "N", "N"],
+          y_true=["Y", "Y", "N", "N", "Y", "N", "Y"],
+          input_type="binary",
+          pos_label="Y",
+          average="binary",
+          expected=_classification.ConfusionMatrix(tp=2, tn=2, fp=1, fn=2),
       ),
       dict(
           testcase_name="binary_micro_average",
@@ -35,9 +50,31 @@ class ClassificationTest(parameterized.TestCase):
           y_true=[1, 1, 0, 0, 1, 0, 1],
           input_type="binary",
           average="micro",
-          expected=_classification.ConfusionMatrixAcc(
-              tp=4, tn=4, fp=3, fn=3, config=dict(average="micro")
-          ),
+          expected=_classification.ConfusionMatrix(tp=4, tn=4, fp=3, fn=3),
+      ),
+      dict(
+          testcase_name="multiclass_indicator_micro_average",
+          y_pred=[[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1], [0, 1]],
+          y_true=[[1, 0], [1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0]],
+          input_type="multiclass-indicator",
+          average="micro",
+          expected=_classification.ConfusionMatrix(tp=4, tn=4, fp=3, fn=3),
+      ),
+      dict(
+          testcase_name="multiclass_micro_average",
+          y_pred=["y", "n", "y", "n", "y", "n", "n", "u"],
+          y_true=["y", "y", "n", "n", "y", "n", "y", "u"],
+          input_type="multiclass",
+          average="micro",
+          expected=_classification.ConfusionMatrix(tp=5, tn=13, fp=3, fn=3),
+      ),
+      dict(
+          testcase_name="multiclass_multioutput_micro_average",
+          y_pred=[["y"], ["n", "y"], ["y"], ["n"], ["y"], ["n"], ["n"], ["u"]],
+          y_true=[["y"], ["y"], ["n"], ["n"], ["y", "n"], ["n"], ["y"], ["u"]],
+          input_type="multiclass-multioutput",
+          average="micro",
+          expected=_classification.ConfusionMatrix(tp=6, tn=12, fp=3, fn=3),
       ),
       dict(
           testcase_name="binary_macro_average",
@@ -45,99 +82,155 @@ class ClassificationTest(parameterized.TestCase):
           y_true=[1, 1, 0, 0, 1, 0, 1],
           input_type="binary",
           average="macro",
-          expected=_classification.ConfusionMatrixAcc(
+          expected=_classification.ConfusionMatrix(
               tp=[2, 2],
               tn=[2, 2],
               fp=[1, 2],
               fn=[2, 1],
-              config=dict(average="macro"),
           ),
       ),
       dict(
-          testcase_name="micro_average",
-          y_pred=[1, 0, 1, 0, 1, 0, 0],
-          y_true=[1, 1, 0, 0, 1, 0, 1],
-          input_type="binary",
-          average="micro",
-          expected=_classification.ConfusionMatrixAcc(
-              tp=4, tn=4, fp=3, fn=3, config=dict(average="micro")
-          ),
-      ),
-      dict(
-          testcase_name="macro_average",
-          y_pred=[1, 0, 1, 0, 1, 0, 0],
-          y_true=[1, 1, 0, 0, 1, 0, 1],
-          input_type="binary",
+          testcase_name="multiclass_indicator_macro_average",
+          y_pred=[[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1], [0, 1]],
+          y_true=[[1, 0], [1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0]],
+          input_type="multiclass-indicator",
           average="macro",
-          expected=_classification.ConfusionMatrixAcc(
+          expected=_classification.ConfusionMatrix(
               tp=[2, 2],
               tn=[2, 2],
               fp=[1, 2],
               fn=[2, 1],
-              config=dict(average="macro"),
           ),
       ),
       dict(
-          testcase_name="samples_average",
+          testcase_name="multiclass_macro_average",
+          y_pred=["y", "n", "y", "n", "y", "n", "n", "u"],
+          y_true=["y", "y", "n", "n", "y", "n", "y", "u"],
+          input_type="multiclass",
+          # A vocab is required to align class index for macro.
+          vocab={"y": 0, "n": 1, "u": 2},
+          average="macro",
+          expected=_classification.ConfusionMatrix(
+              tp=[2, 2, 1],
+              tn=[3, 3, 7],
+              fp=[1, 2, 0],
+              fn=[2, 1, 0],
+          ),
+      ),
+      dict(
+          testcase_name="multiclass_multioutput_macro_average",
+          y_pred=[["y"], ["n", "y"], ["y"], ["n"], ["y"], ["n"], ["n"], ["u"]],
+          y_true=[["y"], ["y"], ["n"], ["n"], ["y", "n"], ["n"], ["y"], ["u"]],
+          input_type="multiclass-multioutput",
+          # A vocab is required to align class index for macro.
+          vocab={"y": 0, "n": 1, "u": 2},
+          average="macro",
+          expected=_classification.ConfusionMatrix(
+              tp=[3, 2, 1],
+              tn=[3, 2, 7],
+              fp=[1, 2, 0],
+              fn=[1, 2, 0],
+          ),
+      ),
+      dict(
+          testcase_name="binary_samples_average",
           y_pred=[1, 0, 1, 0, 1, 0, 0],
           y_true=[1, 1, 0, 0, 1, 0, 1],
           input_type="binary",
           average="samples",
-          expected=_classification.ConfusionMatrixAcc(
+          expected=_classification.ConfusionMatrix(
               tp=[1, 0, 0, 1, 1, 1, 0],
               tn=[1, 0, 0, 1, 1, 1, 0],
               fp=[0, 1, 1, 0, 0, 0, 1],
               fn=[0, 1, 1, 0, 0, 0, 1],
-              config=dict(average="samples"),
+          ),
+      ),
+      dict(
+          testcase_name="multiclass_indicator_samples_average",
+          y_pred=[[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1], [0, 1]],
+          y_true=[[1, 0], [1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0]],
+          input_type="multiclass-indicator",
+          average="samples",
+          expected=_classification.ConfusionMatrix(
+              tp=[1, 0, 0, 1, 1, 1, 0],
+              tn=[1, 0, 0, 1, 1, 1, 0],
+              fp=[0, 1, 1, 0, 0, 0, 1],
+              fn=[0, 1, 1, 0, 0, 0, 1],
+          ),
+      ),
+      dict(
+          testcase_name="multiclass_samples_average",
+          y_pred=["y", "n", "y", "n", "y", "n", "n", "u"],
+          y_true=["y", "y", "n", "n", "y", "n", "y", "u"],
+          input_type="multiclass",
+          average="samples",
+          expected=_classification.ConfusionMatrix(
+              tp=[1, 0, 0, 1, 1, 1, 0, 1],
+              tn=[2, 1, 1, 2, 2, 2, 1, 2],
+              fp=[0, 1, 1, 0, 0, 0, 1, 0],
+              fn=[0, 1, 1, 0, 0, 0, 1, 0],
           ),
       ),
   ])
-  def test_confusion_matrix_binary_encoding(
-      self, y_true, y_pred, input_type, average, expected
+  def test_confusion_matrix(
+      self,
+      y_true,
+      y_pred,
+      input_type,
+      average,
+      expected,
+      vocab=None,
+      pos_label=1,
   ):
-    confusion_matrix = _classification.ConfusionMatrix()
-    self.assertEqual(
-        expected,
-        confusion_matrix(
-            y_true, y_pred, input_type=input_type, average=average
-        ),
+    confusion_matrix = _classification.ConfusionMatrixAggregate(
+        input_type=input_type,
+        average=average,
+        vocab=vocab,
+        pos_label=pos_label,
     )
+    self.assertEqual(expected, confusion_matrix(y_true, y_pred))
 
   def test_confusion_matrix_merge(self):
     y_pred = [1, 0, 1, 0, 1, 0, 0]
     y_true = [1, 1, 0, 0, 1, 0, 1]
-    confusion_matrix = _classification.ConfusionMatrix()
-    baseline = _classification.ConfusionMatrixAcc(
-        tp=1, tn=1, fp=1, fn=1, config=dict(average="binary")
+    confusion_matrix = _classification.ConfusionMatrixAggregate()
+    baseline = _classification.ConfusionMatrix(tp=1, tn=1, fp=1, fn=1)
+    acc = confusion_matrix.add_inputs(baseline, y_true, y_pred)
+    acc_other = confusion_matrix.add_inputs(None, y_true, y_pred)
+    actual = confusion_matrix.merge_accumulators([acc, acc_other])
+    expected = _classification.ConfusionMatrix(tp=5, tn=5, fp=3, fn=5)
+    self.assertEqual(expected, actual)
+
+  def test_confusion_matrix_samplewise_merge(self):
+    y_pred = [1, 0]
+    y_true = [1, 1]
+    baseline = _classification.ConfusionMatrix(
+        tp=[1, 0], tn=[1, 2], fp=[0, 2], fn=[0, 3]
+    )
+    confusion_matrix = _classification.ConfusionMatrixAggregate(
+        average="samples"
     )
     acc = confusion_matrix.add_inputs(baseline, y_true, y_pred)
     acc_other = confusion_matrix.add_inputs(None, y_true, y_pred)
     actual = confusion_matrix.merge_accumulators([acc, acc_other])
-    expected = _classification.ConfusionMatrixAcc(
-        tp=5, tn=5, fp=3, fn=5, config=dict(average="binary")
+    expected = _classification.ConfusionMatrix(
+        tp=[1, 0, 1, 0, 1, 0],
+        tn=[1, 2, 1, 0, 1, 0],
+        fp=[0, 2, 0, 1, 0, 1],
+        fn=[0, 3, 0, 1, 0, 1],
     )
     self.assertEqual(expected, actual)
 
   def test_confusion_matrix_add(self):
-    a = _classification.ConfusionMatrixAcc(tp=4, tn=4, fp=2, fn=4)
-    b = _classification.ConfusionMatrixAcc(tp=1, tn=6, fp=9, fn=2)
-    expected = _classification.ConfusionMatrixAcc(tp=5, tn=10, fp=11, fn=6)
+    a = _classification.ConfusionMatrix(tp=4, tn=4, fp=2, fn=4)
+    b = _classification.ConfusionMatrix(tp=1, tn=6, fp=9, fn=2)
+    expected = _classification.ConfusionMatrix(tp=5, tn=10, fp=11, fn=6)
     self.assertEqual(a + b, expected)
-
-  def test_confusion_matrix_invalid_config(self):
-    a = _classification.ConfusionMatrixAcc(
-        tp=4, tn=4, fp=2, fn=4, config=dict(average="micro")
-    )
-    b = _classification.ConfusionMatrixAcc(
-        tp=1, tn=6, fp=9, fn=2, config=dict(average="macro")
-    )
-    with self.assertRaisesRegex(ValueError, "Incompatile config: "):
-      a + b  # pylint: disable=pointless-statement
 
   def test_confusion_matrix_trues_positives(self):
     y_pred = [1, 0, 1, 0, 1, 0, 0]
     y_true = [1, 1, 0, 0, 1, 0, 1]
-    confusion_matrix = _classification.ConfusionMatrix()
+    confusion_matrix = _classification.ConfusionMatrixAggregate()
     cm = confusion_matrix(y_true, y_pred)
     self.assertEqual(3, cm.p)
     self.assertEqual(4, cm.t)
@@ -145,24 +238,52 @@ class ClassificationTest(parameterized.TestCase):
   def test_confusion_matrix_invalid_input_type(self):
     y_pred = [1, 0, 1, 0, 1, 0, 0]
     y_true = [1, 1, 0, 0, 1, 0, 1]
-    confusion_matrix = _classification.ConfusionMatrix()
+    confusion_matrix = _classification.ConfusionMatrixAggregate(
+        input_type="continuous"
+    )
     with self.assertRaisesRegex(NotImplementedError, "is not supported"):
-      confusion_matrix(y_true, y_pred, input_type="invalid")
+      confusion_matrix(y_true, y_pred)
 
   def test_confusion_matrix_invalid_average_type(self):
     y_pred = [1, 0, 1, 0, 1, 0, 0]
     y_true = [1, 1, 0, 0, 1, 0, 1]
-    confusion_matrix = _classification.ConfusionMatrix()
+    confusion_matrix = _classification.ConfusionMatrixAggregate(
+        average="weighted"
+    )
     with self.assertRaisesRegex(NotImplementedError, " is not supported"):
-      confusion_matrix(y_true, y_pred, average="invalid")
+      confusion_matrix(y_true, y_pred)
+
+  def test_confusion_matrix_binary_average_invalid_input(self):
+    y_pred = ["dog", "cat", "cat", "bird", "tiger"]
+    y_true = ["dog", "cat", "bird", "cat", "tiger"]
+    confusion_matrix = _classification.ConfusionMatrixAggregate(
+        input_type="multiclass-multioutput", average="binary"
+    )
+    with self.assertRaisesRegex(ValueError, "input is not supported"):
+      confusion_matrix(y_true, y_pred)
+
+  def test_confusion_matrix_multiclass_indicator_incorrect_input_shape(self):
+    y_pred = [1, 0, 1, 0, 1, 0, 0]
+    y_true = [1, 1, 0, 0, 1, 0, 1]
+    confusion_matrix = _classification.ConfusionMatrixAggregate(
+        input_type="multiclass-indicator"
+    )
+    with self.assertRaisesRegex(ValueError, "needs to be 2D array"):
+      confusion_matrix(y_true, y_pred)
+
+  def test_confusion_matrix_multiclass_macro_vocab_reqruired(self):
+    confusion_matrix = _classification.ConfusionMatrixAggregate(average="macro")
+    acc = _classification.ConfusionMatrix(tp=1, tn=1, fp=1, fn=1)
+    with self.assertRaisesRegex(ValueError, "Global vocab is needed"):
+      confusion_matrix.merge_accumulators([acc])
 
   def test_confusion_matrix_unknown_tn(self):
     y_pred = [1, 0, 1, 0, 1, 0, 0]
     y_true = [1, 1, 0, 0, 1, 0, 1]
-    confusion_matrix = _classification.ConfusionMatrix(unknown_tn=True)
+    confusion_matrix = _classification.ConfusionMatrixAggregate(unknown_tn=True)
     actual = confusion_matrix(y_true, y_pred)
-    expected = _classification.ConfusionMatrixAcc(
-        tp=2, tn=float("inf"), fp=1, fn=2, config=dict(average="binary")
+    expected = _classification.ConfusionMatrix(
+        tp=2, tn=float("inf"), fp=1, fn=2
     )
     self.assertEqual(expected, actual)
 
