43cb93f0743f84ac7ccb98631da70be5d2e495b1
tf-model-analysis-team <tensorflow-extended-team@google.com>
2024-11-21 17:26:54 -0800

    Remove addons from `__init__.py` as it's deprecated with Eval Saved Model.
    PiperOrigin-RevId: 698972892

--- tensorflow_model_analysis/__init__.py
+++ tensorflow_model_analysis/__init__.py
@@ -63,7 +63,6 @@ try:
   # TODO(b/73882264): The orders should be kept in order to make benchmark on
   # DataFlow work. We need to look into why the import orders matters for the
   # DataFlow benchmark.
-  from tensorflow_model_analysis import addons
   from tensorflow_model_analysis import extractors
   from tensorflow_model_analysis import slicer
   from tensorflow_model_analysis import validators
